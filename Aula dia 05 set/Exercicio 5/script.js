262d89c93a997721efccfe917e11cd27d069aaf8
GBielMendes7 <lgabrielucas765@gmail.com>
2022-09-05 21:40:26 -0300

    ok

---
+++ Aula dia 05 set/Exercicio 5/script.js
@@ -0,0 +1,29 @@
+
+
+document.getElementById("teste").addEventListener("click", function(){
+    carro.marca = document.getElementById("campoMarca").value;
+    carro.modelo = document.getElementById("campoModelo").value;
+    carro.ano = document.getElementById("campoAno").value;
+    carro.cor = document.getElementById("campoCor").value;
+    carro.km = document.getElementById("campoKm").value;
+    carro.valor = document.getElementById("campoValor").value;
+    
+    carro.acelerar();
+})
+    
+let carro = {
+    marca: "",
+    modelo: "",
+    ano: "",
+    cor: "",
+    km: "",
+    valor: "",
+    acelerar(){
+		console.log('Acelerando...');
+	},
+	frear(){
+		console.log('Freando');
+	},
+};
+
+
