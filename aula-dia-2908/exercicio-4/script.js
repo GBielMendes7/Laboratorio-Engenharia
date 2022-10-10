dfd57c41e62c43dcadb10bf75ee6c56894fa9986
GBielMendes7 <99516155+GBielMendes7@users.noreply.github.com>
2022-10-10 16:56:30 -0300

    update

--- aula-dia-2908/exercicio-4/script.js
+++ aula-dia-2908/exercicio-4/script.js
@@ -8,7 +8,7 @@ btnAdicionar.addEventListener("click", function () {
 
     let oldOL = document.getElementById("lista");
     if (oldOL !== null)
-   { document.body.removeChild(oldOL); }
+   { document.getElementById("minhaLista").removeChild(oldOL); }
 
     elementos.sort();
 
@@ -23,5 +23,5 @@ btnAdicionar.addEventListener("click", function () {
         elementoOL.appendChild(elementBR);
     });
 
-    document.body.appendChild(elementoOL);
+    document.getElementById("minhaLista").appendChild(elementoOL);
 });
